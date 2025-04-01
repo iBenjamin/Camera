fd426c6c00df06fcb0f2054d07234a5880c0f5e5
Benjamin Wong <iwangkaimin@gmail.com>
2025-04-01 17:40:30 +0800

    fix:  fix compile error for swift6.1

--- Sources/Internal/Miscellaneous/Typealiases.swift
+++ Sources/Internal/Miscellaneous/Typealiases.swift
@@ -11,6 +11,6 @@
 
 import SwiftUI
 
-public typealias CameraScreenBuilder = (CameraManager, Namespace.ID, _ closeMCameraAction: @escaping () -> ()) -> any MCameraScreen
-public typealias CapturedMediaScreenBuilder = (MCameraMedia, Namespace.ID, _ retakeAction: @escaping () -> (), _ acceptMediaAction: @escaping () -> ()) -> any MCapturedMediaScreen
-public typealias ErrorScreenBuilder = (MCameraError, _ closeMCameraAction: @escaping () -> ()) -> any MCameraErrorScreen
+public typealias CameraScreenBuilder = @MainActor @Sendable (CameraManager, Namespace.ID, _ closeMCameraAction: @escaping () -> ()) -> any MCameraScreen
+public typealias CapturedMediaScreenBuilder = @MainActor @Sendable (MCameraMedia, Namespace.ID, _ retakeAction: @escaping () -> (), _ acceptMediaAction: @escaping () -> ()) -> any MCapturedMediaScreen
+public typealias ErrorScreenBuilder = @MainActor @Sendable (MCameraError, _ closeMCameraAction: @escaping () -> ()) -> any MCameraErrorScreen
